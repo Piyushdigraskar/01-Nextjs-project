fd3d733d6a1915089c1bc7e4e5a71fe3d03fce5b
Piyushdigraskar <piyushdigraskar3@gmail.com>
2024-04-29 15:08:00 +0530

    Adding name to aboutus

--- pages/aboutus/index.js
+++ pages/aboutus/index.js
@@ -1,6 +1,35 @@
-const AboutUs = ()=>{
+import Link from "next/link";
+
+const details = [
+    {
+        id: 1,
+        name: 'Yash',
+        role: 'Senior Developer'
+    },
+    {
+        id: 2,
+        name: 'Vaibhav',
+        role: 'Backend Developer'
+    },
+    {
+        id: 3,
+        name: 'Suresh',
+        role: 'Frontend Developer'
+    }
+
+]
+
+const AboutUs = () => {
     return (
-        <h2>About US...</h2>
+        <ul>
+            {details.map(item => (
+                <li key={item.id}>
+                    <Link href={`./aboutus/${item.id}`}>
+                        <h1>{item.name}</h1>
+                    </Link>
+                </li>
+            ))}
+        </ul>
     );
 }
 
