213aa426f4481cced1f8ea11c7d8c7e8aad1df06
Piyushdigraskar <piyushdigraskar3@gmail.com>
2024-04-29 11:35:46 +0530

    Added Nested Routes

---
+++ pages/aboutus/developer.js
@@ -0,0 +1,5 @@
+const NamePage = ()=>{
+    return <h2>Piyush Vilasrao Digraskar</h2>
+}
+
+export default NamePage;
---
+++ pages/news/something-important.js
@@ -0,0 +1,4 @@
+function DetailsPage() {
+    return <h2>The Details Page</h2>;
+}
+export default DetailsPage;
